b52636f0ee8a9a6ef6b7c591acc937d9b8715969
Sriram Pidugu <srirampidugu999@gmail.com>
2025-07-03 18:58:24 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,18 +3,16 @@ import Link from "next/link";
 
 export default function Hero() {
   return (
-    <section className="relative min-h-screen mx-4 md:mx-16 flex items-center justify-center bg-cream">
+    <section className="relative min-h-screen mx-16 flex items-center justify-center bg-cream">
       <Image
         src="/bggg.webp"
-        alt="Animated background"
+        alt="Ocean background"
         fill
         className="object-cover object-center opacity-80"
-        unoptimized
         priority
       />
-      <div className="absolute inset-0 bg-black/10 md:bg-black/50 z-0"></div>
-
-      <div className="relative z-10 text-center px-4 py-24 max-w-2xl mx-auto flex-col gap-2">
+      <div className="absolute inset-0 bg-black/10 md:bg-black/50"></div>
+      <div className="text-center px-4 py-24 max-w-2xl mx-auto flex-col gap-2">
         <h1 className="font-heading text-4xl md:text-5xl lg:text-6xl font-bold mb-4 text-white drop-shadow-lg">
           Psychological Care for
         </h1>
@@ -27,7 +25,8 @@ export default function Hero() {
         </p>
         <Link
           href="https://docs.google.com/forms/d/e/1FAIpQLSeVPdE8FDjGrnmAJ8kAXzXUTrOz780udwG6noNWHbShZuuIgw/viewform"
-          className="inline-block bg-[#9ab7b0] hover:bg-[#a2bdb7] text-lg font-semibold px-8 py-5 rounded-full shadow-2xl transition-colors backdrop-blur-sm"
+          className="inline-block bg-[#9ab7b0] hover:bg-[#a2bdb7] text-lg font-semibold px-8 py-5 rounded-full shadow-2xl transition-colors  backdrop-blur-sm"
+          style={{ position: "relative", zIndex: 10 }}
         >
           Book a Free Consult
         </Link>
